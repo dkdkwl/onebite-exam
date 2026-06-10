a193e179084b1d3f93abfbef1e6815ca78841bd9
dkdkwl <dkdkwl@naver.comm>
2026-06-10 21:48:40 +0900

    tailwindcss 처음

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-xs text-red-500"> text-xs</div>
+      <div className="text-sm"> text-sm</div>
+      <div className="text-lg"> text-lg</div>
+      <div className="text-xl"> text-xl</div>
+      <div className="text-2xl"> text-2xl</div>
+      <div className="text-[20px] text-red-500"> text-[20px]</div>
+    </>
+  );
+}
+
+export default App;
