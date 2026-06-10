0043768d8b40efa29687db52342bb28ca75d5709
dkdkwl <dkdkwl@naver.comm>
2026-06-10 22:00:16 +0900

    간단하게 txt와 패딩 사용방법

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ function App() {
   return (
     <>
       <div className="text-xs text-red-500"> text-xs</div>
-      <div className="text-sm"> text-sm</div>
-      <div className="text-lg"> text-lg</div>
-      <div className="text-xl"> text-xl</div>
-      <div className="text-2xl"> text-2xl</div>
+      <div className="text-sm text-[rgb(100,10,200)]"> text-sm</div>
+      <div className="text-lg font-bold"> text-lg</div>
+      <div className="text-xl font-extrabold"> text-xl</div>
+      <div className="text-2xl font-black"> text-2xl</div>
       <div className="text-[20px] text-red-500"> text-[20px]</div>
+
+      <div className="bg-amber-500"> bg-amber-500</div>
+
+      <div className="w-full bg-blue-500">box</div>
+
+      <div className="h-50 w-50 bg-red-400 px-5 py-5 pt-5 pr-5 pb-5 pl-5">
+        <div className="h-full w-full bg-blue-500"></div>
+      </div>
     </>
   );
 }
