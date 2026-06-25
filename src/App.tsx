e9a8771146a857fce383e6b00e4f6f4e22eec807
dkdkwl <dkdkwl@naver.comm>
2026-06-25 22:11:48 +0900

    아이콘과 chadcn에 대한 내용 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,77 @@
 import "./App.css";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
+import { ChefHat } from "lucide-react";
+
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
-import { Popover, PopoverContent, PopoverTrigger } from "./components/ui/popover";
+} from "@/components/ui/carousel";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 function App() {
   const isActive = true;
   return (
     <>
-    <Popover>
-      <PopoverTrigger>Open</PopoverTrigger>
-      <PopoverContent>This is the popover content</PopoverContent>
-    </Popover>
+      <ChefHat className="text-primary" />
+      <AlertDialog>
+        <AlertDialogTrigger render={<Button variant="outline" />}>
+          Show Dialog
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </DialogContent>
+      </Dialog>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button>open</Button>
+        </PopoverTrigger>
+        <PopoverContent>This is the popover content</PopoverContent>
+      </Popover>
       <div className="p-4">This is the popover content</div>
-      <Button>Click me</Button>
-      <div className={cn(isActive ? "text-muted" : "text-red-500")}>
-        This is muted text
-      </div>
+
       <div className="text-primary">This is primary text</div>
       <div className="text-destructive">This is destructive text</div>
       <Carousel className="mx-10">
