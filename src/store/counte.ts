ad70a4b1fe71b47c63bf286bc286ff1d582e2e3f
dkdkwl <dkdkwl@naver.comm>
2026-06-30 22:07:27 +0900

    zustand middleware중 combine과 immer에 대해서

--- src/store/counte.ts
+++ src/store/counte.ts
@@ -1,28 +1,41 @@
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+export const useCountStore = create(
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increaseOne: () => {
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decreaseOne: () => {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
 
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      set((store) => ({
-        count: store.count + 1,
-      }));
-    },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+// export const useCountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increaseOne: () => {
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+//     decreaseOne: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
@@ -30,10 +43,10 @@ export const useCount = () => {
 };
 
 export const useIncreaseCount = () => {
-  const increase = useCountStore((store) => store.actions.increase);
+  const increase = useCountStore((store) => store.actions.increaseOne);
   return increase;
 };
 export const useDecreaseCount = () => {
-  const decrease = useCountStore((store) => store.actions.decrease);
+  const decrease = useCountStore((store) => store.actions.decreaseOne);
   return decrease;
 };
