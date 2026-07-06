d0330b869d30c22016d9a3edc0037ed816584843
dkdkwl <dkdkwl@naver.comm>
2026-07-06 20:56:40 +0900

    투두리스트 작업

--- src/pages/todo-list-page.tsx
+++ src/pages/todo-list-page.tsx
@@ -1,27 +1,14 @@
 import TodoEditor from "@/components/ui/todo-list/todo-editor";
 import TodoItem from "@/components/ui/todo-list/todo-item";
-
-const dummyTodos = [
-  {
-    id: 1,
-    content: "Todo 1",
-  },
-  {
-    id: 2,
-    content: "Todo 2",
-  },
-  {
-    id: 3,
-    content: "Todo 3",
-  },
-];
+import { useTodos } from "@/store/todos";
 
 export default function TodoListPage() {
+  const tods = useTodos();
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl font-bold">TodoListPage</h1>
       <TodoEditor></TodoEditor>
-      {dummyTodos.map((todo) => (
+      {tods.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
     </div>
