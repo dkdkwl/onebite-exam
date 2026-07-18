1d84da59d572edad23774552f419ed47ca37c197
dkdkwl <dkdkwl@naver.comm>
2026-07-18 16:56:11 +0900

    tanstackquery 사용법

--- src/pages/todo-list-page.tsx
+++ src/pages/todo-list-page.tsx
@@ -1,14 +1,18 @@
 import TodoEditor from "@/components/ui/todo-list/todo-editor";
 import TodoItem from "@/components/ui/todo-list/todo-item";
-import { useTodos } from "@/store/todos";
+import { useTodosData } from "@/store/todos";
 
 export default function TodoListPage() {
-  const tods = useTodos();
+  const { data: todos, isLoading, error } = useTodosData();
+
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (isLoading) return <div>로딩중입니다.</div>;
+
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl font-bold">TodoListPage</h1>
       <TodoEditor></TodoEditor>
-      {tods.map((todo) => (
+      {todos?.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
     </div>
